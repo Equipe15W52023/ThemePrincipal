78b01b7ab071e64d04fcfc7f3f659f1e28e52f5d
Luca Ruggeri <lucaruggeri18@gmail.com>
2023-09-19 09:24:39 -0400

    ajout enqueue dans fonction

--- functions.php
+++ functions.php
@@ -1,3 +1,11 @@
 <?php
-// Fonctions personnalisées pour le thème
-// ...
+
+function ajouter_styles() {
+
+wp_enqueue_style(   'style-principale',  // identificateur du link css
+                    get_template_directory_uri() . '/style.css',  // enroit où se trouve le fichier style.css
+                    array(), // les fichiers css qui dépendent de style.css
+                    filemtime(get_template_directory() . '/style.css')  // version de notre style.css
+); 
+}
+add_action( 'wp_enqueue_scripts', 'ajouter_styles' );
