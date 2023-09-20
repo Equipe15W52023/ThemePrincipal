9011a0aa3f4bfdc22a71f945ca3926f453a5de69
Luca Ruggeri <lucaruggeri18@gmail.com>
2023-09-19 23:28:09 -0400

    debut affichage des articles

--- front-page.php
+++ front-page.php
@@ -2,8 +2,29 @@
 // Template pour la page d'accueil personnalisée
 get_header();
 
-// Contenu de la page d'accueil personnalisée
-// ...
+// Récupération des articles d'une catégorie particulière
+$args = array(
+    'post_type' => 'post',      // Type de publication : articles
+    'category_name' => 'principal',
+    'posts_per_page' => -1,     // Afficher tous les articles de cette catégorie
+);
+
+$custom_query = new WP_Query($args);
+
+// Boucle pour afficher les articles
+if ($custom_query->have_posts()) :
+    while ($custom_query->have_posts()) :
+        $custom_query->the_post();
+        the_title();
+        the_content();
+        // Contenu de l'article
+        // Vous pouvez personnaliser la mise en page ici
+
+    endwhile;
+    wp_reset_postdata(); // Réinitialise la requête personnalisée
+else :
+    echo 'Aucun article trouvé dans cette catégorie.';
+endif;
 
 get_footer();
 ?>
