e171bde3d8add74c1930b9933d4fa24185d3dbc7
Luca Ruggeri <lucaruggeri18@gmail.com>
2023-09-20 23:06:36 -0400

    Ajout du boouton de changement de categorie

--- front-page.php
+++ front-page.php
@@ -2,16 +2,30 @@
 // Template pour la page d'accueil personnalisée
 get_header();
 
-// Récupération des articles d'une catégorie particulière
+// Vérifiez si la catégorie actuelle est définie dans le stockage local
+if (isset($_COOKIE['currentCategory'])) {
+    $currentCategory = $_COOKIE['currentCategory'];
+} else {
+    $currentCategory = 'design'; // Catégorie par défaut si aucune n'est définie dans le stockage local
+}
+
+// Récupération des articles de la catégorie actuelle
+//IMPORTANT: il faut créer la catégori prog et design pour les articles
 $args = array(
     'post_type' => 'post',
     'orderby' => 'meta_value_num',
     'meta_key' => 'custom_post_order',
-    'order' => 'ASC',    
+    'order' => 'ASC',
+    'category_name' => $currentCategory,
 );
 
 $custom_query = new WP_Query($args);
+?>
+<!-- Bouton pour basculer entre les catégories -->
 
+<button id="toggle-category-button">Basculer les catégories</button>
+
+<?php  
 // Boucle pour afficher les articles
 if ($custom_query->have_posts()) :
     while ($custom_query->have_posts()) :
@@ -29,3 +43,32 @@ endif;
 
 get_footer();
 ?>
+
+<script>
+document.addEventListener('DOMContentLoaded', function () {
+    // Sélectionnez le bouton par son ID
+    var toggleCategoryButton = document.getElementById('toggle-category-button');
+
+    // Fonction pour basculer entre les catégories
+    function toggleCategory() {
+        var currentCategory = localStorage.getItem('currentCategory');
+        if (!currentCategory || currentCategory === 'design') {
+            currentCategory = 'prog'; 
+        } else {
+            currentCategory = 'design'; 
+        }
+
+        // Enregistrez la nouvelle catégorie dans le stockage local
+        localStorage.setItem('currentCategory', currentCategory);
+
+        // Mettez à jour la catégorie actuelle dans le cookie
+        document.cookie = 'currentCategory=' + currentCategory + '; path=/';
+        //on rafraichi la page
+        location.reload();    
+    
+    }
+
+    // Ajoutez un gestionnaire d'événement au bouton
+    toggleCategoryButton.addEventListener('click', toggleCategory);
+});
+</script>
