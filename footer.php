c01ed9a0a3e511e67832738983db818c03f4f7a5
Michelle <e2078172@cmaisonneuve.qc.ca>
2023-09-23 15:13:01 -0400

    début footer

--- footer.php
+++ footer.php
@@ -1,5 +1,18 @@
 <footer>
     <!-- Pied de page, informations de copyright, etc. -->
+    <footer class="bloc_footer">
+        <section class="footer_adresse">
+            <div>
+                <p>adresse</p>
+            </div>
+        </section>
+         <section class="footer_logo">
+            <div>
+                <p>logos</p>
+            </div>
+        </section>
+
+    </footer>
 </footer>
 </body>
 </html>
