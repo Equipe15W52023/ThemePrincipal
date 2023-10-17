e7f5a641f025b651ab753692867c5599bb0a607d
TwistWaker <112021985+TwistWaker@users.noreply.github.com>
2023-10-17 08:37:49 -0400

    création du boutton burger et de ces fonctions

--- header.php
+++ header.php
@@ -49,7 +49,23 @@
 <body class="site">
 
    <header class="site__entete">
+   <div id="mySidenav" class="sidenav">
+  <a id="closeBtn" href="#" class="close">×</a>
+  <ul>
+    <li><a href="#">A propos</a></li>
+    <li><a href="#">Nos services</a></li>
+    <li><a href="#">Témoignages</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+</div>
 
+<a href="#" id="openBtn">
+  <span class="burger-icon">
+    <span></span>
+    <span></span>
+    <span></span>
+  </span>
+</a>
        <section class="menu_recherche">
 
            <div class="logomenu">
