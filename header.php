1b909019f2b1a66af7b333c877b2b3656887d98c
Luca Ruggeri <lucaruggeri18@gmail.com>
2023-11-25 16:49:19 -0500

    Avancement Nav

--- header.php
+++ header.php
@@ -24,82 +24,49 @@
 
  
 
-<?php
-
-// $nouvelle_classe = "";
-
-// if (is_front_page()){
-
-//     $nouvelle_classe = "no-aside";
-
-// }
-
-?>
+<body class="site <?= (is_front_page()?'no-aside':'') ?> ">
+   <header class="site__entete">
+       <section class="menu_recherche">
+           <div class="logomenu">
+               <?php the_custom_logo(); ?>
+            </div>
+        <!-- <input type="checkbox" id="chkMenu"> -->
+        
+        <div class="site__entete_suite">
+            <?php wp_nav_menu(array(
+                "menu" => "entete",
+                "container" => "nav"
+                )) ?>
+<!-- Bouton pour basculer entre les catégories -->
  
-
-<body class="site">
-
-  <header class="site__entete">
-   <div id="mySidenav" class="sidenav">
-  <a id="closeBtn" href="#" class="close">×</a>
-  <ul>
-  </ul>
-</div>
-
-<div class="container nav-container">
+<button id="toggle-category-button">Basculer les catégories</button>
+         
+              
+    </div>
+    
+</section>
+<div class="navbar">
+  <div class="container nav-container">
   <input class="checkbox" type="checkbox" name="burger_menu" id="burger" />
       <div class="hamburger-lines">
         <span class="line line1"></span>
         <span class="line line2"></span>
         <span class="line line3"></span>
       </div>  
-   
+    
     <div class="menu-items">
         <?php
-     
+      
       get_template_part("templates-parts/aside");
-     
+      
       ?>
-     
+      
     </div>
   </div>
-       <section class="menu_recherche">
+</div>
+</nav>
 
-           <div class="logomenu">
 
-               <?php the_custom_logo(); ?>
 
-            </div>
 
-        <!-- <input type="checkbox" id="chkMenu"> -->
-
-        <div class="site__entete_suite">
-
-            <?php wp_nav_menu(array(
-
-                "menu" => "entete",
-
-                "container" => "nav"
-
-                )) ?>
-
-        
-
-        <!-- <label class="burger" for="chkMenu">
-
-            <img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" width="50" height="50"></label> -->
-
-            <nav>
-
-            <section id="site__header__titre">
-
- 
-
-</section>
-
-    </div>
-
-</section>
-
-<script src="header.js"></script>
-</header>
+    
---
+++ templates-parts/aside.php
@@ -0,0 +1,12 @@
+<aside class="site__aside">
+    <h3>Menu secondaire</h3>
+    <?php 
+
+
+
+    
+    wp_nav_menu(array(
+        
+        "container" => "nav"  
+    )); ?>
+</aside>
